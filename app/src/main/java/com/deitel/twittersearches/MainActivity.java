e933ec86797384e96ff8d64d2724b6b57ed4f86a
j3l11234 <297259024@qq.com>
2015-03-19 15:20:14 +0800

    changes to the Twitter Searches app to make a Favorite Website List

--- app/src/main/java/com/deitel/twittersearches/MainActivity.java
+++ app/src/main/java/com/deitel/twittersearches/MainActivity.java
@@ -112,7 +112,8 @@ public class MainActivity extends ListActivity
    private void addTaggedSearch(String query, String tag)
    {
       // get a SharedPreferences.Editor to store new tag/query pair
-      SharedPreferences.Editor preferencesEditor = savedSearches.edit();
+      SharedPreferences.Editor preferencesEditor = savedSearches.edit();   
+      
       preferencesEditor.putString(tag, query); // store current search
       preferencesEditor.apply(); // store the updated preferences
       
@@ -134,8 +135,10 @@ public class MainActivity extends ListActivity
       {
          // get query string and create a URL representing the search
          String tag = ((TextView) view).getText().toString();
-         String urlString = getString(R.string.searchURL) +
-            Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+         String urlString = Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+         if(!urlString.startsWith("http://")){
+        	 urlString = "http://"+urlString;
+         }
          
          // create an Intent to launch a web browser    
          Intent webIntent = new Intent(Intent.ACTION_VIEW, 
@@ -214,9 +217,7 @@ public class MainActivity extends ListActivity
    private void shareSearch(String tag)
    {
       // create the URL representing the search
-      String urlString = getString(R.string.searchURL) +
-         Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
-
+      String urlString = Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
       // create Intent to share urlString
       Intent shareIntent = new Intent();
       shareIntent.setAction(Intent.ACTION_SEND);
